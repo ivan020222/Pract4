6ec48f45b1ade2bfb1c0418ae7a38de27ca7f4fb
ivan020222 <vovki.2105@gmail.com>
2023-04-28 10:53:02 +0300

    2

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+let myImage = document.querySelector('img');
+
+function setUserName(){
+    let myName = prompt('Please enter your name.');
+    if(!myName){
+        setUserName();
+    }else{
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+
+function Slider(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc==='images/chrome.png'){
+        myImage.setAttribute('src', 'images/chrome2.png');
+    }else{
+        myImage.setAttribute('src', 'images/chrome.png');
+    }
+}
+
+let Slides = window.setInterval(Slider, 2000);
+
+myButton.onclick = function(){
+    setUserName();
+}
+
+//myImage.onclick = 
